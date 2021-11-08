a448b3db9cb728ea0a9906b3caa5b4b60373420d
David-Alfons <78115178+David-Alfons@users.noreply.github.com>
2021-11-08 08:49:15 +0200

    uploading local files

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const html = document.documentElement;
+const firstDiv = document.getElementById("showcase");
+const stopAt = firstDiv.offsetHeight;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
+const frameCount = 148;
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+const currentFrame = (index) =>
+  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+    .toString()
+    .padStart(4, "0")}.jpg`;
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image();
+img.src = currentFrame(1);
+canvas.width = 1158;
+canvas.height = 770;
+img.onload = function () {
+  context.drawImage(img, 0, 0);
+};
+
+const updateImage = (index) => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+};
+
+window.addEventListener("scroll", () => {
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - (html.scrollHeight - stopAt);
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+
+  requestAnimationFrame(() => updateImage(frameIndex + 1));
+});
